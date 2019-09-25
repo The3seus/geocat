206b2ba8f890b60e396c2359a491c005ff06e737
Theseus <48400442+The3seus@users.noreply.github.com>
2019-09-25 02:15:12 -0400

    Create geocat.sh

---
+++ geocat.sh
@@ -0,0 +1,6 @@
+# geocat
+# simple IP geolocation lookup script 
+# super simple stupid script.... clone geocat.sh then chmod +- geocat.sh then run ./geocat.sh 
+echo please input ip address to obtain geo information on.
+read varname
+geoiplookup $varname  
